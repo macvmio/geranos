1829ca21e224a788c48c7757be56d3789c5c351a
Tomasz Jarosik <tomek.jarosik@gmail.com>
2024-06-04 23:00:32 +0100

    Implement Pull and Remove method for ImageService

--- pkg/imageservice/svc.go
+++ pkg/imageservice/svc.go
@@ -3,8 +3,13 @@ package imageservice
 import (
 	"context"
 	"fmt"
+	"github.com/google/go-containerregistry/pkg/authn"
+	"github.com/google/go-containerregistry/pkg/name"
+	"github.com/google/go-containerregistry/pkg/v1/remote"
 	"github.com/tomekjarosik/geranos/pkg/layout"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	runtimeapi "k8s.io/cri-api/pkg/apis/runtime/v1"
 	"log"
 )
@@ -41,27 +46,104 @@ func (s *Server) ListImages(ctx context.Context, in *runtimeapi.ListImagesReques
 	return &runtimeapi.ListImagesResponse{Images: images}, nil
 }
 
-func (s *Server) ImageStatus(ctx context.Context, in *runtimeapi.ImageStatusRequest) (*runtimeapi.ImageStatusResponse, error) {
-	return &runtimeapi.ImageStatusResponse{}, nil
+func (s *Server) ImageStatus(ctx context.Context, req *runtimeapi.ImageStatusRequest) (*runtimeapi.ImageStatusResponse, error) {
+	imgPtr := req.GetImage()
+	if imgPtr == nil {
+		return nil, status.Errorf(codes.InvalidArgument, "empty image")
+	}
+	ref, err := name.ParseReference(imgPtr.Image, name.StrictValidation)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "image refereence parse error: %s", err.Error())
+	}
+	lm := layout.NewMapper(s.rootDir)
+	_, err = lm.Read(ctx, ref)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "reading image from disk resulted in error: %s", err.Error())
+	}
+	return &runtimeapi.ImageStatusResponse{
+		Image: &runtimeapi.Image{
+			Id:          ref.String(),
+			RepoTags:    []string{ref.String()},
+			RepoDigests: []string{},
+			Size_:       123, // TODO: Read from real size
+			Uid:         nil,
+			Username:    "todo",
+			Spec:        nil,
+			Pinned:      false,
+		},
+		Info: nil,
+	}, nil
 }
 
 func (s *Server) ImageFsInfo(ctx context.Context, in *runtimeapi.ImageFsInfoRequest) (*runtimeapi.ImageFsInfoResponse, error) {
 	return &runtimeapi.ImageFsInfoResponse{}, nil
 }
 
-func (s *Server) PullImage(ctx context.Context, in *runtimeapi.PullImageRequest) (*runtimeapi.PullImageResponse, error) {
-	return &runtimeapi.PullImageResponse{}, nil
+func (s *Server) PullImage(ctx context.Context, req *runtimeapi.PullImageRequest) (*runtimeapi.PullImageResponse, error) {
+	a := req.GetAuth()
+	opts := make([]remote.Option, 0)
+	if a != nil {
+		// NOTE: a.ServerAddress is not used yet
+		log.Printf("using non-empty auth")
+		opts = append(opts, remote.WithAuth(authn.FromConfig(authn.AuthConfig{
+			Username:      a.Username,
+			Password:      a.Password,
+			Auth:          "", // TODO: what is it?
+			IdentityToken: a.IdentityToken,
+			RegistryToken: a.RegistryToken,
+		})))
+	} else {
+		log.Printf("using default auth")
+		opts = append(opts, remote.WithAuthFromKeychain(authn.DefaultKeychain))
+	}
+	imgPtr := req.GetImage()
+	if imgPtr == nil {
+		return nil, status.Errorf(codes.InvalidArgument, "empty image")
+	}
+	ref, err := name.ParseReference(imgPtr.Image, name.StrictValidation)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "image reference parse error: %s", err.Error())
+	}
+	img, err := remote.Image(ref, opts...)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to pull image: %s", err.Error())
+	}
+	lm := layout.NewMapper(s.rootDir)
+	err = lm.Write(ctx, img, ref)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "image write error: %s", err.Error())
+	}
+	return &runtimeapi.PullImageResponse{
+		ImageRef: ref.String(),
+	}, nil
 }
 
-func (s *Server) RemoveImage(ctx context.Context, in *runtimeapi.RemoveImageRequest) (*runtimeapi.RemoveImageResponse, error) {
+func (s *Server) RemoveImage(ctx context.Context, req *runtimeapi.RemoveImageRequest) (*runtimeapi.RemoveImageResponse, error) {
+	imgPtr := req.GetImage()
+	if imgPtr == nil {
+		return nil, status.Errorf(codes.InvalidArgument, "empty image")
+	}
+	ref, err := name.ParseReference(imgPtr.Image, name.StrictValidation)
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "image reference parse error: %s", err.Error())
+	}
+	lm := layout.NewMapper(s.rootDir)
+	err = lm.Remove(ref)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to remove image: %s", err.Error())
+	}
 	return &runtimeapi.RemoveImageResponse{}, nil
 }
 
 // LoggingInterceptor is an example interceptor that logs requests.
 func LoggingInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
-	log.Printf("Received RPC: %v", info.FullMethod)
+	log.Printf("Received RPC: %v\n", info.FullMethod)
+	log.Printf("  request: %v\n", req)
 	// Pass on to the handler to complete the normal execution of a unary RPC.
-	return handler(ctx, req)
+	res, err := handler(ctx, req)
+	log.Printf("End RPC: %v\n", info.FullMethod)
+	log.Printf("  result: %v, err=%v", res, err)
+	return res, err
 }
 
 func NewImageService(dir string) *Server {
